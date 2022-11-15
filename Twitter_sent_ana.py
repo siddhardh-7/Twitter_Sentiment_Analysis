0d0058863f43286c82c5894a32092cceda0c3a6f
siddhardh-7 <cs20b1063@iiitdm.ac.in>
2022-11-15 23:07:35 +0530

    after importing data

---
+++ Twitter_sent_ana.py
@@ -0,0 +1,8 @@
+# %%
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from jupyterthemes import jtplot
+jtplot.style(theme='monokai', context='notebook', ticks=True, grid=False)
+ 
+# %%
