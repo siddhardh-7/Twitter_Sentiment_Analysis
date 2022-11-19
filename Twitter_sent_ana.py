e49b23ab64955a69972b4c40107a6a7200831f3c
SIDDHARDHA <84370785+siddhardh-7@users.noreply.github.com>
2022-11-19 12:51:53 +0530

    Delete Twitter_sent_ana.py

--- Twitter_sent_ana.py
+++
@@ -1,8 +0,0 @@
-# %%
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from jupyterthemes import jtplot
-jtplot.style(theme='monokai', context='notebook', ticks=True, grid=False)
- 
-# %%
